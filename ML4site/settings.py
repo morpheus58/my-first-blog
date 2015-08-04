7a0e398b9aabaf2a70c4fd71d7909b11e787e9f8
morpheus58 <morya.58@gmail.com>
2015-08-04 01:30:28 +0000

    Updating DB

--- ML4site/settings.py
+++ ML4site/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ML4site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ML5',
+	'USER': 'Morya',
+	'PASSWORD': 'InfiniteCorridor12',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
